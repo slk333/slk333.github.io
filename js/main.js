d5ec5b64cc7bec3c80838a73d01d9df6eaf70aef
Antoine Weber <aweb33@icloud.com>
2018-01-17 01:43:04 +0100

    navbar fixed et centrage vertical

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 var header2 = document.querySelector("h2");
 header2.textContent = "js in action";
 header2.setAttribute("style","color:black");
+header2.onclick = function() {
+  header2.textContent = "you clicked me!";
+}
