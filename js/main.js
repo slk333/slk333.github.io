909b0d7249279592727270de0fed87f851f1eb22
Antoine Weber <aweb33@icloud.com>
2018-02-18 12:52:12 +0100

    suppression de retour à la ligne inutiles

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var header2 = document.querySelector("h2");
-header2.textContent = "js in action";
+header2.textContent = "click me";
 header2.setAttribute("style","color:black");
 header2.onclick = function() {
   header2.textContent = "you clicked me!";
